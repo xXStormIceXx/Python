ee38378ce868ccf92d0f2f7cdea173ec50102403
Ruben Ortiz <ruben.ortiz.db9@gmail.com>
2024-02-01 14:33:51 -0500

    trabajo 1

--- EjercicioClase_RubenOrtiz.py
+++ EjercicioClase_RubenOrtiz.py
@@ -4,11 +4,21 @@
 #----------------si el producto no esta debe mostrar un mensaje informando sobre ello.-------------------#
 
 
-invproduc = {"mascotas":("perros","gatos","aves","peces","reptiles"),
-                      "cantidad":(500,1200,100,50,800)}
-mascotas = invproduc["mascotas"]
+invproduc = {"maxcotas":("perros","gatos","aves","peces","reptiles"),
+              "precios":(500,1200,100,50,800)}
+
 for i in range(5):
-    print(invproduc["mascotas"][i],"\t",invproduc["cantidad"][i])
+    print(invproduc["maxcotas"][i],"\t",invproduc["precios"][i])
     
 mascotas = str(input("Digite la Mascota que desea: "))
 cantidad = int(input(f"digite la cantidad de mascotas que desea llevar: "))
+mascotas = mascotas.capitalize()
+totalapagar = int
+
+if mascotas in invproduc("maxcotas"):
+    pocicionmascota = invproduc["macotas"].index(mascotas)
+    totalapagar = invproduc["precios"][pocicionmascota]
+    totalapagar = totalapagar * cantidad
+    print(f"el total a pagar por las mascotas es: $",totalapagar,)
+else:
+    print("La mascota ingresada no se encuentra en el inventario")
