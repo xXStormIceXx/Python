0e1af790defd6ec8d755281f2b6d4cf99327f614
Ruben Ortiz <ruben.ortiz.db9@gmail.com>
2024-02-05 07:26:39 -0500

    tarea

---
+++ Dia 7/Tipos_de_Datos.py/datos-simples.py
@@ -0,0 +1,20 @@
+"string"
+'string'
+
+"""tus datos son:
+      nombre: lucas
+      apellido: dalto"""
+      
+'''tus datos son:
+      nombre: lucas
+      apellido: dalto'''
+      
+      
+40
+
+40.2
+
+False 
+True 
+
+
---
+++ Dia 7/condicionales/if-else.py
@@ -0,0 +1,8 @@
+
+contraseña_almacenada = "DaltoMaestro"
+contraseña_escrita = '''DaltoMaestro'''
+
+if contraseña_almacenada == contraseña_escrita:
+    print("INICIANDO SESIÒN...")
+else: 
+    print("CONTRASEÑA EQUIVOCADA, INTENTE DE NUEVO")    
