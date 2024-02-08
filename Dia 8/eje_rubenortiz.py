bcec30dd391cd81a43327249e97c631fcaf0e165
Ruben Ortiz <ruben.ortiz.db9@gmail.com>
2024-02-08 07:56:45 -0500

    JSON

---
+++ Dia 8/eje_rubenortiz.py
@@ -0,0 +1,25 @@
+import json
+
+#1. Devuelve un listado con todos los pedidos que se han realizado. Los pedidos deben estar ordenados por la fecha de realización, mostrando en primer lugar los pedidos más recientes.
+with open('data.json','r') as contenido:
+    data = json.load(contenido) 
+print(data)
+#2. Devuelve todos los datos de los dos pedidos de mayor valor.
+
+#3. Devuelve un listado con los identificadores de los clientes que han realizado algún pedido. Tenga en cuenta que no debe mostrar identificadores que estén repetidos.
+
+#4. Devuelve un listado de todos los pedidos que se realizaron durante el año 2017, cuya cantidad total sea superior a 500€.
+
+#5. Devuelve un listado con el nombre y los apellidos de los comerciales que tienen una comisión entre 0.05 y 0.11.
+
+#6. Devuelve el valor de la comisión de mayor valor que existe en la tabla comercial.
+
+#7. Devuelve el identificador, nombre y primer apellido de aquellos clientes cuyo ciudad sea "Sevilla". El listado deberá estar ordenado alfabéticamente por apellidos y nombre.
+
+#8. Devuelve un listado de los nombres de los clientes que empiezan por A y terminan por n y también los nombres que empiezan por P. El listado deberá estar ordenado alfabéticamente.
+
+#9. Devuelve un listado de los nombres de los clientes que  empiezan por A. El listado deberá estar ordenado alfabéticamente.
+
+#10. Devuelve un listado con los nombres de los comerciales que tienen como apellido "Ruiz". Tenga en cuenta que se deberán eliminar los nombres repetidos.
+
+#OPCIONAL : CREAR UN CRUD (CREACION,LECTURA,ACTUALIZACIÓN Y ELIMINACIÓN) DE LOS 3 CONJUNTOS DE DATOS ADENTRO DE DATA.JSON, LOS CUALES DEBEN SER PERSISTENTES DONDE APLIQUE.
